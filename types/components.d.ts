fa1f3aad44c6d5ecceda6f2c2c31637aa3bfeb69
Wycliffe Peart <wycliffepeart@users.noreply.github.com>
2023-02-18 23:11:49 -0500

    up

--- types/components.d.ts
+++ types/components.d.ts
@@ -1,10 +1,11 @@
+import React from "react";
 import {HTMLElement} from "happy-dom";
 
 declare global {
   namespace JSX {
     interface IntrinsicElements {
-      'atlas-icon-home': React.DetailedHTMLProps<React.HTMLAttributes<HTMLElement>, HTMLElement>; // Normal web component
-      'atlas-icon-mail': React.DetailedHTMLProps<React.HTMLAttributes<HTMLElement>, HTMLElement>; // Web component extended from input
+      'atlas-icon-home': React.DetailedHTMLProps<React.HTMLAttributes<HTMLElement>, HTMLElement>;
+      'atlas-icon-mail': React.DetailedHTMLProps<React.HTMLAttributes<HTMLElement>, HTMLElement>;
     }
   }
 }
--- types/cypress.d.ts
+++ types/cypress.d.ts
@@ -10,24 +10,3 @@ declare global {
     }
   }
 }
-
-//# sourceMappingURL=index.d.ts.map
-
-// Augment the Cypress namespace to include type definitions for
-// your custom command.
-// Alternatively, can be defined in cypress/support/component.d.ts
-// with a <reference path="./component" /> at the top of your spec.
-declare global {
-
-  namespace Cypress {
-
-    interface cy {
-      mount: typeof mount;
-
-    }
-
-    interface Chainable {
-      mount: typeof mount;
-    }
-  }
-}
