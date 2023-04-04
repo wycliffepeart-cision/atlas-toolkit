b819f46c96fe6915568ea390cf4372193d47f31a
Wycliffe Peart <wycliffepeart@users.noreply.github.com>
2023-04-04 09:46:39 -0500

    fix

--- atlas.sh
+++ atlas.sh
@@ -4,22 +4,22 @@ npm run build
 
 echo "Manage Worktree"
 
-git worktree add --no-checkout --detach ./package
+
+git worktree add --no-checkout --detach -f ./package
 cp package.json ./package
-cp .github/ ./package -r
-cp dist/ ./package -r
+cp -R dist/ ./package
 
 echo "Commit and push"
 
 cd package/
 git switch -c dist
-git add .github/
 git add dist/
 git add package.json
 git commit -m "package"
-git tag -a next-2 -m "next by wycliffepeart"
-git push origin next
+git tag -a next-5 -m "next by wycliffepeart"
+git push origin next-5
 
 echo "Worktree cleanup"
 
-git worktree remove ./package -f
+rm -rf package
+git worktree remove -f ./package
