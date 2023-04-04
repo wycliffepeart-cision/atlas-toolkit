c4d9b2a022e35f7cbe101c86eb4a869ad3896505
Wycliffe Peart <wycliffepeart@users.noreply.github.com>
2023-04-04 08:57:32 -0500

    tr

---
+++ atlas.sh
@@ -0,0 +1,24 @@
+echo "Build Project"
+
+npm run build
+
+echo "Manage Worktree"
+
+git worktree add --no-checkout --detach ./package
+cp package.json ./package
+cp .github/ ./package -r
+cp dist/ ./package -r
+
+echo "Commit and push"
+
+cd package/
+git switch -c dist
+git add .github/
+git add dist/
+git add package.json
+git commit -m "package"
+git tag -a next-{{date +%s }} -m "next by wycliffepeart"
+git push origin next
+
+echo "Worktree cleanup"
+
